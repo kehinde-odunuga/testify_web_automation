f2197122573809836a5cb6eb6f8a4e6595500938
kehinde-odunuga <omobolaji2@gmail.com>
2025-05-28 11:39:04 +0100

    initializing repo

---
+++ src/Tasks/Task1.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hi there!");
+        System.out.println("My name is Kehinde Odunuga");
+        System.out.println("I am a Software QA engineer");
+        System.out.println("I love to write java codes");
+        System.out.println("Let's connect!!!");
+    }
+}
---
+++ src/com/Kehinde/Main.java
@@ -0,0 +1,7 @@
+package com.Kehinde;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Kehinde");
+    }
+}
